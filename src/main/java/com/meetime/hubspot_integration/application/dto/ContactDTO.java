dd9cadaabb4b500a66aea55e580b6706872b5ca0
Luizgms <lgomes_kiau@hotmail.com>
2025-04-01 19:02:07 -0300

    chore: Add class validations

--- src/main/java/com/meetime/hubspot_integration/application/dto/ContactDTO.java
+++ src/main/java/com/meetime/hubspot_integration/application/dto/ContactDTO.java
@@ -1,17 +1,41 @@
 package com.meetime.hubspot_integration.application.dto;
 
-import lombok.*;
+import com.meetime.hubspot_integration.core.model.Company;
+import com.meetime.hubspot_integration.core.model.valueobjects.PhoneNumber;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+import java.util.List;
+
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContactDTO {
-    private Long id;
-    private String email;
+
+    private String hubspotId;
+
+    @NotEmpty(message = "First name cannot be empty")
     private String firstName;
+
+    @NotEmpty(message = "Last name cannot be empty")
     private String lastName;
-    private String website;
-    private String company;
-    private String phone;
+
+    @Email(message = "Email should be valid")
+    @NotEmpty(message = "Email cannot be empty")
+    private String email;
+
+    private PhoneNumber phone;
+
+    private Company company;
+
+    private List<String> tags;
+
+    @Size(max = 50, message = "Status should be less than or equal to 50 characters")
+    private String status;
 }
