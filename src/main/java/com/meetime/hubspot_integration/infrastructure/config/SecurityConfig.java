64d3968e80d6dd1e8a1388ecd9725014ebcbffd1
Luizgms <lgomes_kiau@hotmail.com>
2025-04-01 14:54:02 -0300

    chore: default version

--- src/main/java/com/meetime/hubspot_integration/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/meetime/hubspot_integration/infrastructure/config/SecurityConfig.java
@@ -10,7 +10,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable())  // Desativa CSRF para testes locais
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
